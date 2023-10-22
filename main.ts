9e3cf8b69ea30d9e11bd4542fe739ae7ac6b19b3
Camix777 <c.linares1@uniandes.edu.co>
2023-10-22 15:52:09 -0500

    commit final

--- main.ts
+++ main.ts
@@ -10,11 +10,23 @@ console.log(series);
 function mostrarSeries(listseries: Serie[]):void{
     for (let serie of listseries){
         let trSerie: HTMLElement = document.createElement("tr");
-        trSerie.innerHTML = `<th scope="row">${serie.id}</th><td><a href=${serie.url}>${serie.name}</a></td>
-        <td>${serie.channel}</td><td>${serie.seasons}</td>`
+
+        // Add an event listener for the link to handle the click
+        let link = document.createElement("a");
+        link.href = "#";
+        link.textContent = serie.name;
+        link.onclick = () => mostrarCarta(serie);
+
+        trSerie.innerHTML = `<th scope="row">${serie.id}</th><td></td>
+        <td>${serie.channel}</td><td>${serie.seasons}</td>`;
+
+        // Append the link element to the second <td>
+        trSerie.children[1].appendChild(link);
+
         tablaSeries.appendChild(trSerie);
     }
 }
+
 
 function promedioTemporadas(listseries: Serie[]):number{
     let totalTemporadas: number = 0;
@@ -27,4 +39,16 @@ let paragraph: HTMLElement = document.getElementById("promedio")!;
 mostrarSeries(series);
 paragraph.innerHTML = `Seasons Average: ${promedioTemporadas(series)}`;
 
+function mostrarCarta(serie: Serie):void{
+    let columna: HTMLElement = document.getElementById("columna")!;
+    let carta: HTMLElement = document.getElementById("div")!;
+    carta.innerHTML = `<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${serie.image}">
+    <div class="card-body">
+      <h5 class="card-title">${serie.name}</h5><br>
+      <p class="card-text">${serie.description}<br><br><a href="${serie.url}">${serie.url}</a></p>
+    </div>
+  </div>`
+    columna.appendChild(carta);
+}
 
--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,21 @@ import { series } from "./data.js";
 var tablaSeries = document.getElementById("series");
 console.log(series);
 function mostrarSeries(listseries) {
+    var _loop_1 = function (serie) {
+        var trSerie = document.createElement("tr");
+        // Add an event listener for the link to handle the click
+        var link = document.createElement("a");
+        link.href = "#";
+        link.textContent = serie.name;
+        link.onclick = function () { return mostrarCarta(serie); };
+        trSerie.innerHTML = "<th scope=\"row\">".concat(serie.id, "</th><td></td>\n        <td>").concat(serie.channel, "</td><td>").concat(serie.seasons, "</td>");
+        // Append the link element to the second <td>
+        trSerie.children[1].appendChild(link);
+        tablaSeries.appendChild(trSerie);
+    };
     for (var _i = 0, listseries_1 = listseries; _i < listseries_1.length; _i++) {
         var serie = listseries_1[_i];
-        var trSerie = document.createElement("tr");
-        trSerie.innerHTML = "<th scope=\"row\">".concat(serie.id, "</th><td><a href=").concat(serie.url, ">").concat(serie.name, "</a></td>\n        <td>").concat(serie.channel, "</td><td>").concat(serie.seasons, "</td>");
-        tablaSeries.appendChild(trSerie);
+        _loop_1(serie);
     }
 }
 function promedioTemporadas(listseries) {
@@ -20,3 +30,9 @@ function promedioTemporadas(listseries) {
 var paragraph = document.getElementById("promedio");
 mostrarSeries(series);
 paragraph.innerHTML = "Seasons Average: ".concat(promedioTemporadas(series));
+function mostrarCarta(serie) {
+    var columna = document.getElementById("columna");
+    var carta = document.getElementById("div");
+    carta.innerHTML = "<div class=\"card\" style=\"width: 18rem;\">\n    <img class=\"card-img-top\" src=\"".concat(serie.image, "\">\n    <div class=\"card-body\">\n      <h5 class=\"card-title\">").concat(serie.name, "</h5><br>\n      <p class=\"card-text\">").concat(serie.description, "<br><br><a href=\"").concat(serie.url, "\">").concat(serie.url, "</a></p>\n    </div>\n  </div>");
+    columna.appendChild(carta);
+}
